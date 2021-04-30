363f244cd1d4f1d577653a133ee87a088e62b698
Juan Pablo Acosta <pabloacosta.b17@gmail.com>
2021-04-29 21:05:01 -0500

    endpoint-services feature created for jobs and authorization

---
+++ src/services/auth.services.js
@@ -0,0 +1,27 @@
+const API_URL_BASE = process.env.REACT_APP_API_URL || "";
+
+const loginAccount = () => {};
+
+const registerAccount = async (user) => {
+  try {
+    const payload = {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    };
+
+    const response = await fetch(`${API_URL_BASE}/api/users`, payload);
+    const newUser = await response.json();
+
+    return newUser;
+  } catch (err) {
+    throw Error("OHHPS");
+  }
+};
+
+const forgotPassword = () => {};
+
+export { loginAccount, registerAccount, forgotPassword };
