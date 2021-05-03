aff07b2061960f0ab3c15cc6074b275f9ffa4b47
Juan Pablo Acosta <pabloacosta.b17@gmail.com>
2021-05-03 17:54:43 -0500

    form register modified from class syntax to const syntax

--- src/components/Sign/Register/Form/index.js
+++ src/components/Sign/Register/Form/index.js
@@ -1,106 +1,102 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component, useState } from "react";
+import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faLock, faUser } from '@fortawesome/fontawesome-free-solid';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faLock,
+  faUser,
+} from "@fortawesome/fontawesome-free-solid";
 
-class FormRegister extends Component {
-  constructor() {
-    super();
+const FormRegister = () => {
+  const [form, setForm] = useState({});
 
-    this.state = {};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(evt) {
-    this.setState({
-      [evt.target.id]: evt.target.value,
-    });
-  }
-
-  handleSubmit(evt) {
+  const handleChange = (evt) => {
     evt.preventDefault();
-    this.props.handleSave(this.state);
-  }
+  };
 
-  validateForm() {
-    const { name, email, password } = this.state;
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+  };
 
-    return (email && email.length > 0) &&
-      (password && password.length > 0) &&
-      (name && name.length > 0);
-  }
+  const validateForm = () => {
+    const { name, email, password } = form;
 
-  render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="form-group">
-          <div className="input-group">
-            <span className="input-group-addon">
-              <FontAwesomeIcon icon={faUser} size="1x" />
-            </span>
-            <input
-              id="name"
-              type="text"
-              className="form-control"
-              placeholder="Your name"
-              required
-              onChange={this.handleChange}
-            />
-          </div>
-        </div>
-
-        <hr className="hr-xs" />
-
-        <div className="form-group">
-          <div className="input-group">
-            <span className="input-group-addon">
-              <FontAwesomeIcon icon={faEnvelope} size="1x" />
-            </span>
-            <input
-              id="email"
-              type="email"
-              className="form-control"
-              placeholder="Email"
-              required
-              onChange={this.handleChange}
-            />
-          </div>
-        </div>
-
-        <hr className="hr-xs" />
-
-        <div className="form-group">
-          <div className="input-group">
-            <span className="input-group-addon">
-              <FontAwesomeIcon icon={faLock} size="1x" />
-            </span>
-            <input
-              id="password"
-              type="password"
-              className="form-control"
-              placeholder="Password"
-              required
-              onChange={this.handleChange}
-            />
-          </div>
-        </div>
-
-        <button
-          className="btn btn-primary btn-block"
-          type="submit"
-          disabled={!this.validateForm()}
-        >
-          Login
-        </button>
-
-      </form>
+      email &&
+      email.length > 0 &&
+      password &&
+      password.length > 0 &&
+      name &&
+      name.length > 0
     );
-  }
-}
+  };
 
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <div className="input-group">
+          <span className="input-group-addon">
+            <FontAwesomeIcon icon={faUser} size="1x" />
+          </span>
+          <input
+            id="name"
+            type="text"
+            className="form-control"
+            placeholder="Your name"
+            required
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+
+      <hr className="hr-xs" />
+
+      <div className="form-group">
+        <div className="input-group">
+          <span className="input-group-addon">
+            <FontAwesomeIcon icon={faEnvelope} size="1x" />
+          </span>
+          <input
+            id="email"
+            type="email"
+            className="form-control"
+            placeholder="Email"
+            required
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+
+      <hr className="hr-xs" />
+
+      <div className="form-group">
+        <div className="input-group">
+          <span className="input-group-addon">
+            <FontAwesomeIcon icon={faLock} size="1x" />
+          </span>
+          <input
+            id="password"
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            required
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+
+      <button
+        className="btn btn-primary btn-block"
+        type="submit"
+        disabled={!validateForm}
+      >
+        Login
+      </button>
+    </form>
+  );
+};
 
 FormRegister.propTypes = {
   handleSave: PropTypes.func.isRequired,
