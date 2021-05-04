3c96c8e4814e86e2b95bd93e19541e412a72c745
Juan Pablo Acosta <pabloacosta.b17@gmail.com>
2021-05-03 21:32:25 -0500

    register new user feature completed

--- src/components/Sign/Register/Form/index.js
+++ src/components/Sign/Register/Form/index.js
@@ -1,7 +1,7 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
-
+import { registerAccount, getEmails } from "../../../../services/auth.services";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faEnvelope,
@@ -11,13 +11,42 @@ import {
 
 const FormRegister = () => {
   const [form, setForm] = useState({});
+  const history = useHistory();
 
   const handleChange = (evt) => {
     evt.preventDefault();
+    const { name, value } = evt.target;
+    setForm({ ...form, [name]: value });
   };
 
-  const handleSubmit = (evt) => {
+  const handleSubmit = async (evt) => {
     evt.preventDefault();
+
+    const newUser = { ...form };
+
+    try {
+      const users = await getEmails();
+      let state = false;
+      users.map((user) => {
+        if (user.email === newUser.email) {
+          state = true;
+        }
+        return state;
+      });
+
+      if (state === true) {
+        alert("este email ya ha sido registrado");
+      } else {
+        const userRegistered = await registerAccount(newUser);
+
+        delete userRegistered.password;
+
+        localStorage.setItem("THE_JOB_APP", JSON.stringify(userRegistered));
+        history.push("/");
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const validateForm = () => {
@@ -32,7 +61,7 @@ const FormRegister = () => {
       name.length > 0
     );
   };
-
+  console.log("hola", form);
   return (
     <form onSubmit={handleSubmit}>
       <div className="form-group">
@@ -42,6 +71,7 @@ const FormRegister = () => {
           </span>
           <input
             id="name"
+            name="name"
             type="text"
             className="form-control"
             placeholder="Your name"
@@ -60,6 +90,7 @@ const FormRegister = () => {
           </span>
           <input
             id="email"
+            name="email"
             type="email"
             className="form-control"
             placeholder="Email"
@@ -78,12 +109,27 @@ const FormRegister = () => {
           </span>
           <input
             id="password"
+            name="password"
             type="password"
             className="form-control"
             placeholder="Password"
             required
             onChange={handleChange}
           />
+        </div>
+      </div>
+
+      <hr className="hr-xs" />
+
+      <div className="form-group">
+        <div className="input-group">
+          <span className="input-group-addon">
+            <FontAwesomeIcon icon={"dice"} size="1x" />
+          </span>
+          <select id="role" name="role" onChange={handleChange}>
+            <option>candidate</option>
+            <option>admin</option>
+          </select>
         </div>
       </div>
 
